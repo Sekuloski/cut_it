4601ef714c71738dfbcfcd1e9bfe638f4ab5a4fb
Sekuloski <sekuloski.bojan18@gmail.com>
2023-07-23 16:59:44 +0200

    feat: Python script refactoring

--- Utilities/plank_generator.py
+++ Utilities/plank_generator.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from PIL import Image
 
 
@@ -30,7 +29,7 @@ def split_images(image_path):
     return images
 
 
-def save_sheet(images):
+def save_sheet(images, name):
     max_width = max(image.width for image in images)
     max_height = max(image.height for image in images)
 
@@ -39,33 +38,29 @@ def save_sheet(images):
     sheet_width = max_width * 5 + spacing * 6  # 5 images with 5 spacings in between
     sheet_height = max_height * 2 + spacing * 3  # 2 rows with 2 spacings in between
 
-    spritesheet = Image.new("RGBA", (sheet_width, sheet_height), (0, 0, 0, 0))
+    sheet = Image.new("RGBA", (sheet_width, sheet_height), (0, 0, 0, 0))
 
     y_offset_top = spacing
     y_offset_bottom = max_height + 2 * spacing
     for i in range(5):
         # Top row
         x_offset_top = i * (max_width + spacing) + spacing
-        spritesheet.paste(images[i], (x_offset_top, y_offset_top))
+        sheet.paste(images[i], (x_offset_top, y_offset_top))
 
         # Bottom row
         x_offset_bottom = i * (max_width + spacing) + spacing
-        spritesheet.paste(images[i + 5], (x_offset_bottom, y_offset_bottom))
+        sheet.paste(images[i + 5], (x_offset_bottom, y_offset_bottom))
 
-    spritesheet.save('results/spritesheet.png')
+    sheet.save(f'results/{name} Sprites.png')
 
 
 def main():
     root = Path(__file__).resolve().parent.parent
-    results = Path(__file__).resolve().parent / 'results'
-    image_path = root / 'Assets' / 'Images' / 'Inital Plank.png'
-    images = split_images(image_path)
-    counter = 0
-    # for url, image in images.items():
-    #     counter += 1
-        # image.save(results / url)
-    images = dict(sorted(images.items()))
-    save_sheet(list(images.values()))
+    name = 'Initial Plank'
+    image_path = root / 'Assets' / 'Resources' / 'Planks' / f'{name}.png'
+    images = dict(sorted(split_images(image_path).items()))
+
+    save_sheet(list(images.values()), name)
 
 
 if __name__ == '__main__':
